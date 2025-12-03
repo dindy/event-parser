84d222c002eeed5d22cbfa66ac2bd4cc8ef9319c
Sylvain Saint-André <sylvain.saintandre@gmail.com>
2025-12-03 11:00:42 +0100

    Cleaning and refactoring

--- app.js
+++ app.js
@@ -1,120 +1,16 @@
 const path = require('path');
 const express = require("express");
-const log = require('node-file-logger');
 const cors = require('cors');
-const scrapper = require('./scrapper.js');
-const fbParser = require('./parsers/event/facebook-event-parser.js');
-const fbGroupParser = require('./parsers/group/facebook-group-parser.js');
-const instaGroupParser = require('./parsers/group/instagram-group-parser.js');
-const defaultGroupParser = require('./parsers/group/default-group-parser.js');
-const instaParser = require('./parsers/event/instagram-event-parser.js');
-const defaultParser = require('./parsers/event/default-event-parser.js');
-const helloassoParser = require('./parsers/event/helloasso-event-parser.js');
-const shotgunParser = require('./parsers/event/shotgun-event-parser.js');
+const { scrap } = require('./controllers/scrapper.js');
 
 global.appRootDir = path.resolve(__dirname);
 global.appDebugDir = global.appRootDir + '/debug/';
+
 const app = express();
-app.use(cors());
 const port = 3001;
 
-const scrapEvent = async (url, provider) => {
-
-    let parser = null;
-
-    const metas = {
-      startTimestamp: null,
-      endTimestamp: null,
-      description: null,
-      place: null,
-      ticketsUrl: null,
-      address: null,
-      hosts: [],
-      url: null,
-      online: null,
-      physicalAddress: {
-        description: null,
-        geom: null,
-        locality: null,
-        postalCode: null,
-        street: null,
-        country: null,
-      },
-      og: []
-    };    
-    
-    switch (provider) {
-        case 'facebook':
-            parser = fbParser;
-            break;
-        case 'instagram':
-            parser = instaParser;
-            break;
-        case 'helloasso':
-            parser = helloassoParser;
-            break;
-        case 'shotgun':
-            parser = shotgunParser;
-            break;
-        default:
-            parser = defaultParser;
-            break;
-    }
-    console.log(url);
-    
-    return await scrapper.scrap(url, parser, metas);    
-}
-
-const scrapGroup = async (url, provider) => {
-
-    let parser = null;
-    const metas = {
-        logos: [],
-        banners: [],
-        name: null,
-        url: null,
-        description: null,
-        physicalAddress: null
-    }
-    
-    switch (provider) {
-        case 'facebook':
-            parser = fbGroupParser;
-            break;
-        case 'instagram':
-            parser = instaGroupParser;
-            break;
-        default:
-            parser = defaultGroupParser;
-            break;
-    }
-
-    return await scrapper.scrap(url, parser, metas);    
-}
-
-app.get("/scrap", async (req, res) => {
-
-    const query = req.query;
-    const url = query.url;
-    const provider = query.provider;
-    const type = query.type;
-    
-    if (!query.url) return req.reject();
-
-    let buff = new Buffer(url, 'base64');
-    let decodedUrl = buff.toString('ascii');
-
-    log.Info('Request for ' + decodedUrl);
-    let data = null
-    if (type == 'event') {
-        data = await scrapEvent(decodedUrl, provider)
-    } else {
-        data = await scrapGroup(decodedUrl, provider)
-    }
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({data}));
-});
-
+app.use(cors());
+app.get("/scrap", scrap);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`App listening on port ${port}!`);
 });
---
+++ controllers/scrapper.js
@@ -0,0 +1,108 @@
+const scrapper = require('../libs/scrapper.js');
+const fbParser = require('../parsers/event/facebook-event-parser.js');
+const fbGroupParser = require('../parsers/group/facebook-group-parser.js');
+const instaGroupParser = require('../parsers/group/instagram-group-parser.js');
+const defaultGroupParser = require('../parsers/group/default-group-parser.js');
+const instaParser = require('../parsers/event/instagram-event-parser.js');
+const defaultParser = require('../parsers/event/default-event-parser.js');
+const helloassoParser = require('../parsers/event/helloasso-event-parser.js');
+const shotgunParser = require('../parsers/event/shotgun-event-parser.js');
+const log = require('node-file-logger');
+
+const scrapEvent = async (url, provider) => {
+
+    let parser = null;
+
+    const metas = {
+      startTimestamp: null,
+      endTimestamp: null,
+      description: null,
+      place: null,
+      ticketsUrl: null,
+      address: null,
+      hosts: [],
+      url: null,
+      online: null,
+      physicalAddress: {
+        description: null,
+        geom: null,
+        locality: null,
+        postalCode: null,
+        street: null,
+        country: null,
+      },
+      og: []
+    };    
+    
+    switch (provider) {
+        case 'facebook':
+            parser = fbParser;
+            break;
+        case 'instagram':
+            parser = instaParser;
+            break;
+        case 'helloasso':
+            parser = helloassoParser;
+            break;
+        case 'shotgun':
+            parser = shotgunParser;
+            break;
+        default:
+            parser = defaultParser;
+            break;
+    }
+    
+    return await scrapper.scrap(url, parser, metas);    
+}
+
+const scrapGroup = async (url, provider) => {
+
+    let parser = null;
+    const metas = {
+        logos: [],
+        banners: [],
+        name: null,
+        url: null,
+        description: null,
+        physicalAddress: null
+    }
+    
+    switch (provider) {
+        case 'facebook':
+            parser = fbGroupParser;
+            break;
+        case 'instagram':
+            parser = instaGroupParser;
+            break;
+        default:
+            parser = defaultGroupParser;
+            break;
+    }
+
+    return await scrapper.scrap(url, parser, metas);    
+}
+
+module.exports = {
+    scrap: async (req, res) => {
+
+        const query = req.query;
+        const url = query.url;
+        const provider = query.provider;
+        const type = query.type;
+        
+        if (!query.url) return req.reject();
+
+        let buff = new Buffer(url, 'base64');
+        let decodedUrl = buff.toString('ascii');
+
+        log.Info('Request for ' + decodedUrl);
+        let data = null
+        if (type == 'event') {
+            data = await scrapEvent(decodedUrl, provider)
+        } else {
+            data = await scrapGroup(decodedUrl, provider)
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ data }));
+    }
+}
