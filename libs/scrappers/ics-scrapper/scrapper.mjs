9a7fc83935de7293035693986b23948980c2e1f0
Sylvain Saint-André <sylvain.saintandre@gmail.com>
2026-02-25 12:19:44 +0100

    Increase ics scraper timeout

--- libs/scrappers/ics-scrapper/scrapper.mjs
+++ libs/scrappers/ics-scrapper/scrapper.mjs
@@ -3,7 +3,7 @@ import ical from 'node-ical'
 export const scrap = async url =>
 {
     const abortCtrl = new AbortController()
-    setTimeout(() => abortCtrl.abort(), 5_000)
+    setTimeout(() => abortCtrl.abort(), 15_000)
 
     return await ical.async.fromURL(url, { signal: abortCtrl.signal })
 }        
